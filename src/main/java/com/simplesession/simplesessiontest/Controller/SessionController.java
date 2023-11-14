4d7548798722d287c42d9da068f79e42df50ac6d
ghkdtlwns987 <ghkdtlwns987@naver.com>
2023-11-14 14:52:03 +0900

    v2

--- src/main/java/com/simplesession/simplesessiontest/Controller/SessionController.java
+++ src/main/java/com/simplesession/simplesessiontest/Controller/SessionController.java
@@ -6,14 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpSession;
 import java.util.Date;
+import java.util.Enumeration;
 
 /**
  *
  */
 @Controller
-@RequestMapping("/")
+@RequestMapping("/session")
 public class SessionController {
-    @GetMapping("/")
+    @GetMapping("/print")
     public String printSession(HttpSession session, Model model){
         System.out.println("Session : " + session);
         System.out.println("Session Id : " + session.getId());
@@ -24,5 +25,24 @@ public class SessionController {
         model.addAttribute("session_id", session.getId());
         model.addAttribute("session_timestamp" , current_Timestamp);
         return "session/print";
+    }
+
+    @GetMapping("/refresh")
+    public String refershSession(HttpSession session, Model model){
+        session.invalidate();
+        return "session/done";
+    }
+
+    @GetMapping("/getAllSession")
+    public String getAllSession(HttpSession session, Model model){
+        int number = 0;
+        Enumeration<?> attrName = session.getAttributeNames();
+        while (attrName.hasMoreElements()) {
+            String attr = (String) attrName.nextElement();
+            System.out.println(session.getAttribute(attr));
+            model.addAttribute("count", attr);
+        }
+
+        return "session/all";
     }
 }
